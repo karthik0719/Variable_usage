70593bcd8c357175fc4e951ed3eacbb489d57256
karthik0719 <104984916+karthik0719@users.noreply.github.com>
2022-05-09 20:30:38 +0530

    Add files via upload

---
+++ variable_usage.py
@@ -0,0 +1,30 @@
+"""
+Name: Karthik
+Date: 9th May 2022
+Day: Monday
+Time: 8:00-8:30PM
+File Description:  Variables Usage
+"""
+'''1st Way of assigning the values and printing them'''
+p=2 
+q=3
+r=4
+s=5
+print(p)
+print(q)
+print(r)
+print(s)
+print(p,q,r,s)
+
+# 2nd Way of assigning the values and printing them
+p=2; q=3;  r=4; s=5
+print(p); print(q); print(r); print(s)
+print(p,q,r,s)
+
+# 3rd  Way of assigning the values and printing them
+p=r=4; q=2; s=5
+print(p); print(q); print(r); print(s)
+
+# 4th Way of assigning the values and printing them
+p,q,r,s=1,2,3,4
+print(p); print(q); print(r); print(s)
